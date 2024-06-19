a0d22ff2a043f3055f55c3f84a9e9f55e0f6eb11
Bunny <bunnycdy@gmail.com>
2024-06-19 21:44:00 +0545

    google login service added

--- config/services.php
+++ config/services.php
@@ -41,5 +41,11 @@ return [
         'redirect' => env('FACEBOOK_REDIRECT_URI'),
     ],
 
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_CALLBACK_URL'),
+    ],
+
 
 ];
