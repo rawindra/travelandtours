98aa6c41ee18917674bf91c39fa3a8a0558a85c3
Bunny <bunnycdy@gmail.com>
2024-06-22 11:49:38 +0545

    relationship added in reviews

--- app/Models/Reviews.php
+++ app/Models/Reviews.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Reviews extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'user_id',
+        'review',
+        'rating',
+    ];
+
+    public function product() : BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
