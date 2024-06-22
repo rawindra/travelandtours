f18cfce5e8c636ea027c0663cd89fd339b7fa00b
Bunny <bunnycdy@gmail.com>
2024-06-22 17:38:08 +0545

    reviews relationship added in Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
@@ -14,5 +15,10 @@ class Product extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function reviews() : HasMany
+    {
+        return $this->hasMany(Reviews::class);
     }
 }
